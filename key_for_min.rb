4f0d51759fc6e8e3d7a60e54f482a164bf79bb9e
adam n <adam.nawroc@gmail.com>
2018-03-02 13:08:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,13 @@ def key_for_min_value(name_hash)
 # hold the prev value
 
   num = 1000
-  x = name_hash.each do |k,v|
+  x = []
+  name_hash.each do |k,v|
     if v < num
 
       num = v
-      ans = k
-      ans
+      x.push(k)
+      # ans
       # puts ans
     end
   end
