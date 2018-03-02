76a5ea0db1dec6033d9f7c3354a41ce4df171168
adam n <adam.nawroc@gmail.com>
2018-03-02 13:08:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -21,4 +21,4 @@ def key_for_min_value(name_hash)
   puts x.inspect
 end
 
-key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})
+key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})
